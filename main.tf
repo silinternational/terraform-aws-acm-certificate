c6dba52debb6d8c7456c1a48cc4dffb5f811dbab
Matt H <matt_henderson@sil.org>
2023-06-15 16:49:47 -0400

    Remove index key from aws_acm_certificate, since not using `count`

--- main.tf
+++ main.tf
@@ -15,15 +15,15 @@ resource "aws_acm_certificate" "this" {
 resource "cloudflare_record" "validation" {
   count = var.create_dns_validation ? 1 : 0
 
-  name    = tolist(aws_acm_certificate.this[0].domain_validation_options)[0].resource_record_name
-  value   = tolist(aws_acm_certificate.this[0].domain_validation_options)[0].resource_record_value
-  type    = tolist(aws_acm_certificate.this[0].domain_validation_options)[0].resource_record_type
+  name    = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_name
+  value   = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_value
+  type    = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_type
   zone_id = data.cloudflare_zone.this.id
   proxied = false
 }
 
 resource "aws_acm_certificate_validation" "this" {
   count                   = var.create_dns_validation ? 1 : 0
-  certificate_arn         = aws_acm_certificate.this[0].arn
+  certificate_arn         = aws_acm_certificate.this.arn
   validation_record_fqdns = [cloudflare_record.validation[0].hostname]
 }
