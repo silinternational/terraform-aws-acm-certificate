a355ec4e345f479742f6de053de471c8236b9847
Matt H <matt_henderson@sil.org>
2023-06-15 16:07:23 -0400

    Define required versions

---
+++ versions.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = ">= 2.0.0, < 4.0.0"
+    }
+  }
+}
