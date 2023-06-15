93f2f1d348743d320d498cf4c29a6cb826f2d7eb
Matt H <matt_henderson@sil.org>
2023-06-15 16:40:20 -0400

    Fix formatting of outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 
 output "certificate_arn" {
   description = "The ARN of the AWS ACM Certificate this created"
-  value = aws_acm_certificate.this.arn
+  value       = aws_acm_certificate.this.arn
 }
