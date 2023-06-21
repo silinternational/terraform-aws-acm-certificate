00a54cecfd677b3c7c302bd72a5c7fc6d7c51b8a
Matt H <matt_henderson@sil.org>
2023-06-21 10:50:05 -0400

    Fix reference to resource that might not exist (since it has a `count`)

--- outputs.tf
+++ outputs.tf
@@ -6,5 +6,5 @@ output "certificate_arn" {
 
 output "validation_id" {
   description = "The time at which the certificate was issued"
-  value       = aws_acm_certificate_validation.this.id
+  value       = one(aws_acm_certificate_validation.this).id
 }
